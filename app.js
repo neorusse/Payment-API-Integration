9fe8952850e29ef77b4fc3cc35306ce9f57c4779
Russell Nyorere <40567910+neorusse@users.noreply.github.com>
2018-09-21 02:21:03 +0100

    typo update

--- app.js
+++ app.js
@@ -2,8 +2,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const exphbs = require('express-handlebars');
-let ravepay = require('ravepay');
-let rave = new ravepay('FLWPUBK-266ccc849f9191b1b09d7b82f608c3e8-X', 'FLWSECK-1123f2745fc997334844ba75608fb566-X', true);
 
 // initializing our app
 const app = express();
